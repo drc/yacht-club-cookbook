6dee23f2fa1ec2103e0b814d7ab05004a9ac9801
Dan Cigrang <danielcigrang@me.com>
2020-04-20 10:24:17 -0500

    working slug dynamic, need to move to categories

--- next.config.js
+++ next.config.js
@@ -1,25 +1,3 @@
-const fs = require("fs");
-const appetizersFolder = "./content/appetizers";
-
-const getPathForAppetizers = (category) => {
-  return fs
-    .readdirSync(`./content/${category}`)
-    .map((app) => {
-      const trimmedName = app.substring(0, app.length - 3);
-      return {
-        [`/recipes/${category}/${trimmedName}`]: {
-          page: `/recipes/${category}/[slug]`,
-          query: {
-            slug: trimmedName,
-          },
-        },
-      };
-    })
-    .reduce((acc, curr) => {
-      return { ...acc, ...curr };
-    }, {});
-};
-
 module.exports = {
   webpack: (configuration) => {
     configuration.module.rules.push({
@@ -28,12 +6,4 @@ module.exports = {
     });
     return configuration;
   },
-  async exportPathMap(defaultPathMap) {
-    console.log(defaultPathMap);
-    return {
-      ...defaultPathMap,
-      ...getPathForAppetizers("appetizers"),
-    };
-  },
 };
-// console.log(module.exports.exportPathMap);
--- pages/recipes/appetizers/[slug].jsx
+++ pages/recipes/appetizers/[slug].jsx
@@ -1,12 +1,10 @@
 import Layout from "../../../components/layout";
+import fs from "fs";
+import path from "path";
 
-export default function AppetizersDetail(props) {
-  const { recipe } = props;
-  const { html } = recipe || "";
-  const { title, thumbnail } = recipe?.attributes || {
-    title: "",
-    thumbnail: "",
-  };
+export default function AppetizersDetail({recipe}) {
+  const { html } = recipe;
+  const { title, thumbnail } = recipe?.attributes;
 
   return (
     <Layout>
@@ -20,11 +18,28 @@ export default function AppetizersDetail(props) {
   );
 }
 
-AppetizersDetail.getInitialProps = async function ({ query }) {
-  const { slug } = query;
-  console.log(slug);
-  const recipe = await import(`../../../content/appetizers/${slug}.md`).catch(
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join("content", "appetizers"));
+  const paths = files.map((file) => ({
+    params: {
+      slug: file.replace(".md", ""),
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const obj = await import(`../../../content/appetizers/${slug}.md`).catch(
     (error) => null
   );
-  return { recipe };
-};
+
+  const recipe = JSON.parse(JSON.stringify(obj));
+
+  return {
+    props: {recipe},
+  };
+}
